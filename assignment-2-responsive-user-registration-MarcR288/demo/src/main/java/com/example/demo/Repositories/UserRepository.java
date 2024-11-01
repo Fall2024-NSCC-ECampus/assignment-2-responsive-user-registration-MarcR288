9ba3e8e153e3b98330aca4136b11d365a7686dec
Marc Remillard <160653970+MarcR288@users.noreply.github.com>
2024-11-01 15:00:46 -0300

    Committing Responsive LOGIN
    Adding Login Functionality

---
+++ assignment-2-responsive-user-registration-MarcR288/demo/src/main/java/com/example/demo/Repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+
+}
